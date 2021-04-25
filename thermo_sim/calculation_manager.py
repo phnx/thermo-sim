c49c477589ae710444393a37014d3403ee424fa8
Ping Charoenwet <wachiraph.c@gmail.com>
2021-04-25 11:24:06 +0700

    try importing CoolProp

--- thermo_sim/calculation_manager.py
+++ thermo_sim/calculation_manager.py
@@ -1,10 +1,11 @@
 # from os import environ
 
+import CoolProp.CoolProp as CP
 
 # def retrieving_constant():
     # CONSTANT = environ.get('CONSTANT')
 
-
 def calculate(var_1: float, var_2: float):
+    # CP can be used here with 2 input variables
     return var_1 + var_2
             
