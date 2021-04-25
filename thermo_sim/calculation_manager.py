cd6b178b84ab640984c7a013f6a83d482db87e11
Joke <smallfrog99@hotmail.com>
2021-04-25 20:11:55 +0700

    edit calculation

--- thermo_sim/calculation_manager.py
+++ thermo_sim/calculation_manager.py
@@ -1,13 +1,17 @@
 # from os import environ
 
 import CoolProp.CoolProp as CP
+# from CoolProp.CoolProp import PhaseSI, PropsSI, get_global_param_string
 
 # def retrieving_constant():
     # CONSTANT = environ.get('CONSTANT')
 
 def calculate_1(var_1: float, var_2: float) -> float:
     # CP can be used here with 2 input variables
-    return var_1 + var_2
+    a = var_1/100*(100-a) + a
+    b = CP.PropsSI("T", "P", var_1, "Q", 0, "Water")
+    a = a + b + 1
+    return b
 
 def calculate_2(var_1: float, var_2: float) -> float:
     # CP can be used here with 2 input variables
@@ -16,4 +20,3 @@ def calculate_2(var_1: float, var_2: float) -> float:
 def calculate_3(var_1: float, var_2: float) -> float:
     # CP can be used here with 2 input variables
     return var_1 - var_2
-
